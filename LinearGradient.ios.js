d7ae5eac60ae28944e6e75812e983a8600b308fa
Brent Vatne <brent.vatne@madriska.com>
2015-05-23 15:09:59 -0700

    Change to new style of requireNativeComponent

--- LinearGradient.ios.js
+++ LinearGradient.ios.js
@@ -5,19 +5,7 @@
 
 'use strict';
 
-var ReactNativeViewAttributes = require('ReactNativeViewAttributes');
-var createReactNativeComponentClass = require('createReactNativeComponentClass');
-var merge = require('merge');
-var deepDiffer = require('deepDiffer');
-
-var LinearGradient = createReactNativeComponentClass({
-  validAttributes: merge(ReactNativeViewAttributes.UIView, {
-    colors:    {diff: deepDiffer},
-    start:     {diff: deepDiffer},
-    end:       {diff: deepDiffer},
-    locations: {diff: deepDiffer},
-  }),
-  uiViewClassName: 'BVLinearGradient',
-});
+var { requireNativeComponent, } = require('react-native');
+var LinearGradient = requireNativeComponent('BVLinearGradient', null);
 
 module.exports = LinearGradient;

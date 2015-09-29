1a2cd1c4f20ae182781bcbbd41aa5bb7cab71ecc
Nick Hudkins <nick@nickhudkins.com>
2015-09-29 12:43:22 -0400

    color conversion in JS

--- LinearGradient.ios.js
+++ LinearGradient.ios.js
@@ -5,7 +5,17 @@
 
 'use strict';
 
-var { requireNativeComponent, } = require('react-native');
-var LinearGradient = requireNativeComponent('BVLinearGradient', null);
+var React = require('react-native');
+var { requireNativeComponent, processColor } = React;
+var NativeLinearGradient = requireNativeComponent('BVLinearGradient', null);
+
+var LinearGradient = React.createClass({
+  render: function() {
+    var { colors, ...otherProps } = this.props;
+    return (
+      <NativeLinearGradient {...otherProps} colors={colors.map(processColor)} />
+    );
+  }
+});
 
 module.exports = LinearGradient;

e7bba10302946a4ee3eb904046d40a2f769de50a
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2016-01-19 12:51:35 +0200

    do not declare variable in createViewManagers

--- android/src/main/java/com/BV/LinearGradient/LinearGradientPackage.java
+++ android/src/main/java/com/BV/LinearGradient/LinearGradientPackage.java
@@ -6,7 +6,7 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -17,13 +17,14 @@ public class LinearGradientPackage implements ReactPackage {
         return Collections.emptyList();
     }
 
+    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
+    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        List<ViewManager> modules = new ArrayList<>();
-        modules.add(new LinearGradientManager());
-        return modules;
+        return Arrays.<ViewManager>asList(
+            new LinearGradientManager());
     }
 }

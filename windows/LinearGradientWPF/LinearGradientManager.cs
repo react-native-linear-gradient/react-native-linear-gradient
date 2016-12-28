3577177f2fa74e27ae81f07f4fee6d89737c8067
Kevin VanGelder <kevin@infinite.red>
2016-12-28 12:10:01 -0800

    initial implementation for wpf - needs RNW node module

---
+++ windows/LinearGradientWPF/LinearGradientManager.cs
@@ -0,0 +1,60 @@
+﻿using ReactNative.UIManager;
+using System;
+using System.Collections.Generic;
+using System.Windows.Media;
+using Newtonsoft.Json.Linq;
+using ReactNative.UIManager.Annotations;
+
+namespace LinearGradient
+{
+    class LinearGradientManager : SimpleViewManager<LinearGradientView>
+    {
+        public const String REACT_CLASS = "BVLinearGradient";
+        public const String PROP_COLORS = "colors";
+        public const String PROP_LOCATIONS = "locations";
+        public const String PROP_START_POS = "start";
+        public const String PROP_END_POS = "end";
+
+        private LinearGradientBrush _linearGradient;
+
+        public override string Name
+        {
+            get
+            {
+                return REACT_CLASS;
+            }
+        }
+
+        protected override LinearGradientView CreateViewInstance(ThemedReactContext reactContext)
+        {
+            return new LinearGradientView();
+        }
+
+        [ReactProp(PROP_COLORS)]
+        public void setColors(LinearGradientView linearGradient, List<string> colors)
+        {
+            linearGradient.setColors(colors);
+        }
+
+        [ReactProp(PROP_LOCATIONS)]
+        public void setLocations(LinearGradientView linearGradient, List<float> locations)
+        {
+            if (locations != null)
+            {
+                linearGradient.setLocations(locations);
+            }
+        }
+
+        [ReactProp(PROP_START_POS)]
+        public void setStartPosition(LinearGradientView linearGradient, JObject startPos)
+        {
+            linearGradient.setStartPosition(startPos.Value<float>("x"), startPos.Value<float>("y"));
+        }
+
+        [ReactProp(PROP_END_POS)]
+        public void setEndPosition(LinearGradientView linearGradient, JObject endPos)
+        {
+            linearGradient.setEndPosition(endPos.Value<float>("x"), endPos.Value<float>("y"));
+        }
+    }
+}
---
+++ windows/LinearGradientWPF/LinearGradientView.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using ReactNative.UIManager;
+
+namespace LinearGradient
+{
+    class LinearGradientView : Canvas
+    {
+        private LinearGradientBrush _linearGradient;
+        private Canvas _canvas;
+
+        public LinearGradientView() : base()
+        {
+            _linearGradient = new LinearGradientBrush
+            {
+                SpreadMethod = GradientSpreadMethod.Pad
+            };
+            Background = _linearGradient;
+        }
+
+        public void setColors(List<string> colors)
+        {
+            GradientStopCollection stops = _linearGradient.GradientStops;
+            for (int i = 0; i < colors.Count; i++)
+            {
+                GradientStop stop = i < stops.Count ? stops[i] : new GradientStop();
+                stop.Color = ColorHelpers.Parse(Convert.ToUInt32(colors[i]));
+                if (i < stops.Count) stops.RemoveAt(i);
+                stops.Insert(i, stop);
+            }
+            _linearGradient.GradientStops = stops;
+        }
+
+        public void setLocations(List<float> locations)
+        {
+            GradientStopCollection stops = _linearGradient.GradientStops;
+            for (int i = 0; i < locations.Count; i++)
+            {
+                GradientStop stop = i < stops.Count ? stops[i] : new GradientStop();
+                stop.Offset = locations[i];
+                if (i < stops.Count) stops.RemoveAt(i);
+                stops.Insert(i, stop);
+            }
+            _linearGradient.GradientStops = stops;
+        }
+
+        public void setStartPosition(float x, float y)
+        {
+            _linearGradient.StartPoint = new Point(x, y);
+        }
+
+        public void setEndPosition(float x, float y)
+        {
+            _linearGradient.EndPoint = new Point(x, y);
+        }
+    }
+}

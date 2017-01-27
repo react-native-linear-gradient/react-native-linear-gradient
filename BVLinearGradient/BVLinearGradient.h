a98331f5cccf7eb906b7f0a206a68b6e9ff932b5
Enguerrand des Vaux <enguerrand.desvaux@gmail.com>
2017-01-27 16:17:35 +1000

    Update BVLinearGradient.h

--- BVLinearGradient/BVLinearGradient.h
+++ BVLinearGradient/BVLinearGradient.h
@@ -1,5 +1,12 @@
 #import <UIKit/UIKit.h>
+// import RCTView
+#if __has_include(<React/RCTView.h>)
 #import <React/RCTView.h>
+#elif __has_include("RCTView.h")
+#import "RCTView.h"
+#else
+#import "React/RCTView.h"   // Required when used as a Pod in a Swift project
+#endif
 
 @interface BVLinearGradient : RCTView
 

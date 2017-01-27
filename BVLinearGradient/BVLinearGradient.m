0da8715f3d3f34f02cdadbb4854e8a33ec69e547
Enguerrand des Vaux <enguerrand.desvaux@gmail.com>
2017-01-27 16:18:08 +1000

    Update BVLinearGradient.m

--- BVLinearGradient/BVLinearGradient.m
+++ BVLinearGradient/BVLinearGradient.m
@@ -1,5 +1,12 @@
 #import "BVLinearGradient.h"
+// import RCTConvert
+#if __has_include(<React/RCTConvert.h>)
 #import <React/RCTConvert.h>
+#elif __has_include("RCTConvert.h")
+#import "RCTConvert.h"
+#else
+#import "React/RCTConvert.h"   // Required when used as a Pod in a Swift project
+#endif
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
 

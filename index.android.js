416b84a72b9f36b6d5cfbe020b0fbe70d9c65792
Timur Gibadullin <tim.gibadullin@gmail.com>
2017-06-29 15:50:59 +0300

    Replace PropTypes from react with form prop-types package

--- index.android.js
+++ index.android.js
@@ -1,6 +1,7 @@
 // @flow
 
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { processColor, requireNativeComponent, PointPropType, StyleSheet, View } from 'react-native';
 const deprecatedPropType = require('react-native/Libraries/Utilities/deprecatedPropType.js');
 

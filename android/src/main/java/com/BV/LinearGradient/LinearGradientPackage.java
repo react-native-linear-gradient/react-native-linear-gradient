13c83331c78fde576c1abb2e848e399d0791c642
Radek Czemerys <radko93@gmail.com>
2017-08-02 19:39:02 +0200

    Added comment

--- android/src/main/java/com/BV/LinearGradient/LinearGradientPackage.java
+++ android/src/main/java/com/BV/LinearGradient/LinearGradientPackage.java
@@ -17,6 +17,7 @@ public class LinearGradientPackage implements ReactPackage {
         return Collections.emptyList();
     }
 
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }

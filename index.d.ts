1aab8683d3207a6bd028c77f1070275723818811
Sebastian Luberriaga <sebastian_lube@live.com.ar>
2017-12-26 12:31:20 -0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -5,8 +5,8 @@ declare module "react-native-linear-gradient" {
 
     interface LinearGradientProps extends ReactNative.ViewProperties {
         colors: string[],
-        start?: { x: number, y: number },
-        end?: { x: number, y: number },
+        startPoint?: { x: number, y: number },
+        endPoint?: { x: number, y: number },
         locations?: number[]
     }
 

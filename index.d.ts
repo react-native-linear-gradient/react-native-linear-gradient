9c51c8ed0d2894961a592598ce50757e930d7508
Bruno Lemos <brunolemos@users.noreply.github.com>
2018-05-28 03:49:31 -0400

    Fix index.d.ts
    Partially revert #278
    Extend ViewProps instead of ViewProperties

--- index.d.ts
+++ index.d.ts
@@ -3,14 +3,12 @@ import * as ReactNative from "react-native";
 
 declare module "react-native-linear-gradient" {
 
-    export interface LinearGradientProps extends ReactNative.ViewProperties {
+    export interface LinearGradientProps extends ReactNative.ViewProps {
         colors: string[],
         startPoint?: { x: number, y: number },
         endPoint?: { x: number, y: number },
         locations?: number[]
     }
 
-    export class LinearGradient extends React.Component<LinearGradientProps, any> { }
-
-    export default LinearGradient
+    export default class LinearGradient extends React.Component<LinearGradientProps, any> { }
 }

76cb1e94f94444b198873d8a8670bf786d1c7446
Eric <eric@lugg.com>
2018-06-18 19:01:22 -0700

    take colors as array of ColorPropTypes so it doesnt error out when passing a color that was processed with normalizeColor or setNormalizedColorAlpha

--- index.ios.js
+++ index.ios.js
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 import { processColor, PointPropType, View, ViewPropTypes } from 'react-native';
 import type { ViewProps } from 'react-native/Libraries/Components/View/ViewPropTypes';
 const deprecatedPropType = require('react-native/Libraries/Utilities/deprecatedPropType.js');
+const ColorPropType = require('react-native/Libraries/StyleSheet/ColorPropType.js');
 
 import NativeLinearGradient from './nativeLinearGradient';
 
@@ -48,7 +49,7 @@ export default class LinearGradient extends Component {
         'Use point object with {x, y} instead.'
       )
     ]),
-    colors: PropTypes.arrayOf(PropTypes.string).isRequired,
+    colors: PropTypes.arrayOf(ColorPropType).isRequired,
     locations: PropTypes.arrayOf(PropTypes.number),
     ...ViewPropTypes,
   };

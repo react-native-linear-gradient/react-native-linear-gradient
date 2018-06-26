f797368d242a56fca943e8acd0216a2c2196e5a9
Brett Jurgens <brett@brettjurgens.com>
2018-06-26 10:58:53 -0500

    update typescript definition

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ import * as ReactNative from "react-native";
 
 declare module "react-native-linear-gradient" {
 
-    export interface LinearGradientProps extends ReactNative.ViewProperties {
+    export type LinearGradientProps = Exclude<ReactNative.ViewProperties, 'start' | 'end'> & {
         colors: (string|number)[],
         start?: { x: number, y: number },
         end?: { x: number, y: number },

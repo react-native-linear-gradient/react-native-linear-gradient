9d860d03b2254a5088eacefc12685403beea689d
henrikra <henrik.raitasola@gmail.com>
2018-10-16 12:46:52 +0300

    Do not use ViewProperties because they are deprecated

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ declare module 'react-native-linear-gradient' {
   import * as React from 'react';
   import * as ReactNative from 'react-native';
 
-  export interface LinearGradientProps extends ReactNative.ViewProperties {
+  export interface LinearGradientProps extends ReactNative.ViewProps {
     colors: (string | number)[];
     start?: { x: number; y: number };
     end?: { x: number; y: number };

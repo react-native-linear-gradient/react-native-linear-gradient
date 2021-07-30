c647a6861adb75d79a64c66c7855661d29da8840
André Mota <andre.mota@glintt.com>
2021-07-30 22:04:15 +0100

    feat: added required x and y proptypes to start and end

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-import { Platform } from "react-native";
-import LinearGradientIos from "./index.ios.js";
-import LinearGradientAndroid from "./index.android.js";
+import LinearGradientAndroid from './index.android.js';
+import LinearGradientIos from './index.ios.js';
+import { Platform } from 'react-native';
+import PropTypes from 'prop-types';
 
-const LinearGradient = Platform.OS === "ios"
-  ? LinearGradientIos
-  : LinearGradientAndroid;
+const LinearGradient =
+  Platform.OS === 'ios' ? LinearGradientIos : LinearGradientAndroid;
 
+LinearGradient.propTypes = {
+  end: PropTypes.shape({
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired,
+  }),
+  start: PropTypes.shape({
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired,
+  }),
+};
 export default LinearGradient;

48e49e70fad9a6b8190ab3cd7b9c493998380cbd
Sunbreak <sunbreak.wang@gmail.com>
2023-02-27 19:22:50 +0800

    Support Fabric on Android

--- android/src/main/java/com/rnlineargradient/LinearGradientManager.java
+++ android/src/main/java/com/rnlineargradient/LinearGradientManager.java
@@ -1,12 +1,15 @@
 package com.rnlineargradient;
 
+import androidx.annotation.NonNull;
+
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
-import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.uimanager.ThemedReactContext;
 import com.facebook.react.uimanager.annotations.ReactProp;
 
-public class LinearGradientManager extends SimpleViewManager<LinearGradientView> {
+@ReactModule(name = LinearGradientManager.REACT_CLASS)
+public class LinearGradientManager extends LinearGradientManagerSpec<LinearGradientView> {
     public static final String REACT_CLASS = "RNLinearGradient";
     public static final String PROP_COLORS = "colors";
     public static final String PROP_LOCATIONS = "locations";
@@ -17,21 +20,25 @@ public class LinearGradientManager extends SimpleViewManager<LinearGradientView>
     public static final String PROP_ANGLE = "angle";
     public static final String PROP_BORDER_RADII = "borderRadii";
 
+    @NonNull
     @Override
     public String getName() {
         return REACT_CLASS;
     }
 
+    @NonNull
     @Override
-    protected LinearGradientView createViewInstance(ThemedReactContext context) {
+    protected LinearGradientView createViewInstance(@NonNull ThemedReactContext context) {
         return new LinearGradientView(context);
     }
 
+    @Override
     @ReactProp(name = PROP_COLORS)
     public void setColors(LinearGradientView gradientView, ReadableArray colors) {
         gradientView.setColors(colors);
     }
 
+    @Override
     @ReactProp(name = PROP_LOCATIONS)
     public void setLocations(LinearGradientView gradientView, ReadableArray locations) {
         if (locations != null) {
@@ -39,26 +46,31 @@ public class LinearGradientManager extends SimpleViewManager<LinearGradientView>
         }
     }
 
+    @Override
     @ReactProp(name = PROP_START_POINT)
     public void setStartPoint(LinearGradientView gradientView, ReadableMap startPoint) {
         gradientView.setStartPoint(startPoint);
     }
 
+    @Override
     @ReactProp(name = PROP_END_POINT)
     public void setEndPoint(LinearGradientView gradientView, ReadableMap endPoint) {
         gradientView.setEndPoint(endPoint);
     }
 
+    @Override
     @ReactProp(name = PROP_USE_ANGLE, defaultBoolean = false)
     public void setUseAngle(LinearGradientView gradientView, boolean useAngle) {
         gradientView.setUseAngle(useAngle);
     }
 
+    @Override
     @ReactProp(name = PROP_ANGLE_CENTER)
     public void setAngleCenter(LinearGradientView gradientView, ReadableMap in) {
         gradientView.setAngleCenter(in);
     }
 
+    @Override
     @ReactProp(name = PROP_ANGLE, defaultFloat = 45.0f)
     public void setAngle(LinearGradientView gradientView, float angle) {
         gradientView.setAngle(angle);
@@ -66,6 +78,7 @@ public class LinearGradientManager extends SimpleViewManager<LinearGradientView>
 
     // temporary solution until following issue is resolved:
     // https://github.com/facebook/react-native/issues/3198
+    @Override
     @ReactProp(name = PROP_BORDER_RADII)
     public void setBorderRadii(LinearGradientView gradientView, ReadableArray borderRadii) {
         gradientView.setBorderRadii(borderRadii);

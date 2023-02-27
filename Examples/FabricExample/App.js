cb4236380b56dff7539255f36be1bdea2ebc974f
Sunbreak <sunbreak.wang@gmail.com>
2023-02-27 10:56:45 +0800

    Link FabricExample with local package

--- Examples/FabricExample/App.js
+++ Examples/FabricExample/App.js
@@ -3,110 +3,109 @@
  * https://github.com/facebook/react-native
  *
  * @format
- * @flow strict-local
+ * @flow
  */
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+ import React, {Component, Fragment} from 'react';
+ import {
+   SafeAreaView,
+   StyleSheet,
+   ScrollView,
+   View,
+   Text,
+   StatusBar,
+ } from 'react-native';
+ 
+ import {Header, Colors} from 'react-native/Libraries/NewAppScreen';
+ 
+ import LinearGradient from 'react-native-linear-gradient';
+ 
+ class App extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       count: 0,
+       colorTop: '#000000',
+       colorBottom: '#cccccc',
+     };
+   }
+ 
+   incrementColor = (color, step) => {
+     const intColor = parseInt(color.substr(1), 16);
+     const newIntColor = (intColor + step).toString(16);
+     return `#${'0'.repeat(6 - newIntColor.length)}${newIntColor}`;
+   };
+ 
+   componentDidMount() {
+     setInterval(() => {
+       this.setState({
+         count: this.state.count + 1,
+         colorTop: this.incrementColor(this.state.colorTop, 1),
+         colorBottom: this.incrementColor(this.state.colorBottom, -1),
+       });
+     }, 20);
+   }
+ 
+   render() {
+     return (
+       <Fragment>
+         <StatusBar barStyle="dark-content" />
+         <SafeAreaView>
+           <ScrollView
+             contentInsetAdjustmentBehavior="automatic"
+             style={styles.scrollView}>
+             <Header />
+             {global.HermesInternal == null ? null : (
+               <View style={styles.engine}>
+                 <Text style={styles.footer}>Engine: Hermes</Text>
+               </View>
+             )}
+             <View style={styles.body}>
+               <LinearGradient
+                 colors={[this.state.colorTop, this.state.colorBottom]}
+                 style={styles.gradient}
+               />
+               <Text style={{color: this.state.colorTop}}>
+                 {this.state.colorTop}
+               </Text>
+               <Text style={{color: this.state.colorBottom}}>
+                 {this.state.colorBottom}
+               </Text>
+             </View>
+           </ScrollView>
+         </SafeAreaView>
+       </Fragment>
+     );
+   }
+ }
+ 
+ const styles = StyleSheet.create({
+   scrollView: {
+     backgroundColor: Colors.lighter,
+   },
+   engine: {
+     position: 'absolute',
+     right: 0,
+   },
+   body: {
+     backgroundColor: Colors.white,
+   },
+   highlight: {
+     fontWeight: '700',
+   },
+   gradient: {
+     width: 200,
+     height: 200,
+   },
+   footer: {
+     color: Colors.dark,
+     fontSize: 12,
+     fontWeight: '600',
+     padding: 4,
+     paddingRight: 12,
+     textAlign: 'right',
+   },
+ });
+ 
+ export default App;
+ 

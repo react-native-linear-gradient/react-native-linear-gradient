32a58c183dc43ab12130394dbf0512b9de8a7bba
Sunbreak <sunbreak.wang@gmail.com>
2023-02-28 10:19:24 +0800

    Remove module-resolver failure

--- Examples/FabricExample/babel.config.js
+++ Examples/FabricExample/babel.config.js
@@ -1,17 +1,3 @@
-const path = require('path');
-const pak = require('../../package.json');
-
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        extensions: ['.tsx', '.ts', '.js', '.json'],
-        alias: {
-          [pak.name]: path.join(__dirname, '../..', pak.source),
-        },
-      },
-    ],
-  ],
 };

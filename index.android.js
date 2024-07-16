a8e87dbbd68c81bee455b5743a24a297ba13e2b1
MazurDorian <mazur.dorian15@gmail.com>
2024-07-16 18:14:29 +0200

    fix: react-native 0.74 processColor

--- index.android.js
+++ index.android.js
@@ -3,7 +3,7 @@
  * @flow
  */
 import React, { Component, createRef } from 'react';
-import { processColor, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import NativeLinearGradient, { type Props } from './src';
 
@@ -85,7 +85,7 @@ export default class LinearGradient extends Component<Props> {
       <View ref={this.gradientRef} {...otherProps} style={style}>
         <NativeLinearGradient
           style={{position: 'absolute', top: 0, left: 0, bottom: 0, right: 0}}
-          colors={colors.map(processColor)}
+          colors={colors}
           startPoint={convertPoint('start', start)}
           endPoint={convertPoint('end', end)}
           locations={locations ? locations.slice(0, colors.length) : null}

b60f0b570b6913f64b96b279ac9a1965a4b42845
Frieder Bluemle <frieder.bluemle@gmail.com>
2026-01-09 08:57:31 -0800

    Consolidate and combine platform specific code

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-import { Platform } from 'react-native';
-import LinearGradientIos from './index.ios.js';
-import LinearGradientAndroid from './index.android.js';
-
-const LinearGradient =
-  Platform.OS === 'ios' ? LinearGradientIos : LinearGradientAndroid;
-
-export default LinearGradient;
+export { default } from './src';
